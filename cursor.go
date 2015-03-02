ad0e4762823386e9794aeaf16dccb0cfe764edf3
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-02 15:10:03 +0900

    Implement cursor back and cursor forward

--- cursor.go
+++ cursor.go
@@ -6,6 +6,16 @@ import (
 	"fmt"
 )
 
+// Moves the cursor n cells to left.
+func CursorBack(n int) {
+	fmt.Printf("\x1b[%dD", n)
+}
+
+// Moves the cursor n cells to right.
+func CursorForward(n int) {
+	fmt.Printf("\x1b[%dC", n)
+}
+
 // Move cursor horizontally to x.
 func CursorHorizontalAbsolute(x int) {
 	fmt.Printf("\x1b[%dG", x)
--- cursor_windows.go
+++ cursor_windows.go
@@ -6,6 +6,27 @@ import (
 	"unsafe"
 )
 
+func CursorBack(n int) {
+	cursorMove(-1*n, 0)
+}
+
+func CursorForward(n int) {
+	cursorMove(n, 0)
+}
+
+func cursorMove(x int, y int) {
+	handle := syscall.Handle(os.Stdout.Fd())
+
+	var csbi consoleScreenBufferInfo
+	procGetConsoleScreenBufferInfo.Call(uintptr(handle), uintptr(unsafe.Pointer(&csbi)))
+
+	var cursor coord
+	cursor.x = csbi.cursorPosition.x + short(x)
+	cursor.y = csbi.cursorPosition.y + short(y)
+
+	procSetConsoleCursorPosition.Call(uintptr(handle), uintptr(*(*int32)(unsafe.Pointer(&cursor))))
+}
+
 func CursorHorizontalAbsolute(x int) {
 	handle := syscall.Handle(os.Stdout.Fd())
 
