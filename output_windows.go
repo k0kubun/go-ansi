047b11471daa823644d7097b14f7e319d977c53d
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-02 11:38:50 +0900

    Write escape sequence parser

--- output_windows.go
+++ output_windows.go
@@ -34,14 +34,55 @@ func (w *Writer) Write(data []byte) (n int, err error) {
 		n += size
 
 		switch ch {
-		case '\\':
-			w.trapBashSlash(r)
+		case '\x1b':
+			size, err = w.handleEscape(r)
+			n += size
+			if err != nil {
+				break
+			}
 		default:
-			fmt.Print(string(ch))
+			fmt.Fprint(w.out, string(ch))
 		}
 	}
 	return
 }
 
-func (w *Writer) trapBashSlash(r *bytes.Reader) {
+func (w *Writer) handleEscape(r *bytes.Reader) (n int, err error) {
+	buf := make([]byte, 0, 10)
+	buf = append(buf, "\x1b"...)
+
+	// Check '[' continues after \x1b
+	ch, size, err := r.ReadRune()
+	if err != nil {
+		fmt.Fprint(w.out, string(buf))
+		return
+	}
+	n += size
+	if ch != '[' {
+		fmt.Fprint(w.out, string(buf))
+		return
+	}
+
+	// Parse escape code
+	var code rune
+	argBuf := make([]byte, 0, 10)
+	for {
+		ch, size, err = r.ReadRune()
+		if err != nil {
+			fmt.Fprint(w.out, string(buf))
+			return
+		}
+		n += size
+		if ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') {
+			code = ch
+			break
+		}
+		argBuf = append(argBuf, string(ch)...)
+	}
+
+	w.applyEscapeCode(buf, string(argBuf), code)
+	return
+}
+
+func (w *Writer) applyEscapeCode(buf []byte, arg string, code rune) {
 }
