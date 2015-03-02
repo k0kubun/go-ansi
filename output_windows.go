1c86e1d54dbcabcabfb2c59554fa9ea8a993f48e
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-02 10:46:16 +0900

    Create writer for windows

--- output_windows.go
+++ output_windows.go
@@ -1,14 +1,47 @@
 package ansi
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"os"
 )
 
+type Writer struct {
+	out io.Writer
+}
+
 func NewAnsiStdout() io.Writer {
-	return os.Stdout
+	return &Writer{
+		out: os.Stdout,
+	}
 }
 
 func NewAnsiStderr() io.Writer {
-	return os.Stderr
+	return &Writer{
+		out: os.Stderr,
+	}
+}
+
+func (w *Writer) Write(data []byte) (n int, err error) {
+	r := bytes.NewReader(data)
+
+	for {
+		ch, size, err := r.ReadRune()
+		if err != nil {
+			break
+		}
+		n += size
+
+		switch ch {
+		case '\\':
+			w.trapBashSlash(r)
+		default:
+			fmt.Print(string(ch))
+		}
+	}
+	return
+}
+
+func (w *Writer) trapBashSlash(r *bytes.Reader) {
 }
